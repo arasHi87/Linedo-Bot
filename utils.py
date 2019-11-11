c1bd1566c1381f825abc34a0b622939bf2f10d61
arasHi87 <arasi27676271@gmail.com>
2019-11-11 13:40:16 +0800

    format code style

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from config.config import *
 from imgurpython import ImgurClient
 
+
 def upload_to_imgur(url):
     client_id = IMGUR_CLIENT_ID
     client_secret = IMGUR_CLIENT_SECRET
@@ -8,6 +9,7 @@ def upload_to_imgur(url):
     refresh_token = IMGUR_REFRESH_TOKEN
     client = ImgurClient(client_id, client_secret, access_token, refresh_token)
     return client.upload_from_url(url)['link']
+
 
 if __name__ == '__main__':
     upload_to_imgur('http://img.wenku8.com/image/1/1587/1587s.jpg')
